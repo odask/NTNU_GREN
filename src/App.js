d0ed187da05045cccea091d6c5be0691272a72fe
Oda Steinland Skaug <odaskaug@dhcp-10-22-21-60.wlan.ntnu.no>
2021-09-24 09:20:08 +0200

    New filestructure, modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Treemap from './components/Treemap';
 import data from './components/data';
+import Grid from "@material-ui/core/Grid";
 //import ProgressBar from 'react-bootstrap/ProgressBar'
 
 function App() {
   return (
-    <div className="App">
-      <Treemap data={data} height={400} width={600} />
+    <div className="App" style= {{marginTop: 100}}>
+      <Grid container spacing={2} direction="column" justifyContent="space-around" alignItems="center">
+        <Treemap data={data} height={600} width={1000} />
+      </Grid>
     </div>
   );
 }
